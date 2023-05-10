30047a33d6626b1b4c9618ac1ee9a602150307b4
achrefboosterbc <111418041+achrefboosterbc@users.noreply.github.com>
2023-05-10 21:45:20 +0100

    init

---
+++ app/src/main/java/com/example/video_playerapp/Example.java
@@ -0,0 +1,14 @@
+package com.example.video_playerapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Example extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example);
+    }
+}
